5f9e0beca2dcc62b75583efb0d24ed59809fcadb
Jeff Geerling <geerlingguy@mac.com>
2020-10-08 15:19:57 -0500

    Lint code automatically.

--- bell-slap.py
+++ bell-slap.py
@@ -7,30 +7,27 @@ import RPi.GPIO as GPIO
 from time import sleep
 
 try:
-  # Set up GPIO
-  servo_pin = 21 # GPIO Pin where servo is connected
-  GPIO.setmode(GPIO.BCM)
+    # Set up GPIO
+    servo_pin = 21  # GPIO Pin where servo is connected
+    GPIO.setmode(GPIO.BCM)
 
-  # Define the Pin numbering type and define Servo Pin as output pin
-  GPIO.setup(servo_pin, GPIO.OUT)
-  p = GPIO.PWM(servo_pin, 50) # PWM channel at 50 Hz frequency
-  p.start(0) # Zero duty cycle initially
+    # Define the Pin numbering type and define Servo Pin as output pin
+    GPIO.setup(servo_pin, GPIO.OUT)
+    p = GPIO.PWM(servo_pin, 50)  # PWM channel at 50 Hz frequency
+    p.start(0)  # Zero duty cycle initially
 
-  # Slap the bell.
-  sleep(0.4)
-  p.ChangeDutyCycle(12)
-  sleep(0.4)
-  p.ChangeDutyCycle(9)
-  sleep(0.25)
-  p.ChangeDutyCycle(12)
-  sleep(0.4)
+    # Slap the bell.
+    sleep(0.4)
+    p.ChangeDutyCycle(12)
+    sleep(0.4)
+    p.ChangeDutyCycle(9)
+    sleep(0.25)
+    p.ChangeDutyCycle(12)
+    sleep(0.4)
 
 except KeyboardInterrupt:
-  print("User stopped script during execution.")
-
-except:
-  print("Couldn't use GPIO for some reason.")
+    print("User stopped script during execution.")
 
 finally:
-  # End GPIO session.
-  GPIO.cleanup()
+    # End GPIO session.
+    GPIO.cleanup()
